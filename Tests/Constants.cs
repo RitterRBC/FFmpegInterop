ee24eb68400f4ec580db46fc8e22b741eb59c082
RitterRBC <thorsten.daempfert@gmail.com>
2024-09-24 11:57:06 +0200

    Update subs

--- Tests/Constants.cs
+++ Tests/Constants.cs
@@ -21,10 +21,10 @@ namespace UnitTest
     public static class Constants
     {
         public static string DownloadUriSource = "http://video.ch9.ms/ch9/1fb8/cf59a951-45df-4aee-ae26-c87d37d01fb8/IntegrativeMomandWindowsPhoneAppStudio_mid.mp4";
-        public static int DownloadUriLength = 89328;
+        public static int DownloadUriLength = 89327;
         public static string DownloadStreamedFileName = "d8c317bd-9fbb-4c5f-94ed-501f09841917.mp4";
 
-        public static string StreamingUriSource = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov";
+        public static string StreamingUriSource = "http://83.56.31.69:80/mjpg/video.mjpg";
         public static int StreamingUriLength = 596458;
     }
 }
